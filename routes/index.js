d26714137462b564e7b037565f071a068bffe735
DhanushM12 <dhanushmukhi@gmail.com>
2022-08-01 13:59:00 +0530

    Added update and delete

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,47 @@ router.get('/', async (req, res) =>{
     }
 })
 
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber);
+})
 
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({message: "Deleted subscriber"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if(req.body.name != null){
+        res.subscriber.name = req.body.name
+    }
+    if(req.body.channel != null){
+        res.subscriber.channel = req.body.channel
+    }
+    try {
+        const updatedSub = await res.subscriber.save();
+        res.status(200).json({ message: updatedSub})
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+})
+
+async function getSubscriber(req, res, next){
+    let subscriber;
+    try {
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null){
+            res.status(404).json({ message: 'Cannot find the subscriber'})
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message})
+    }
+    res.subscriber = subscriber;
+    next();
+}
 
 
 
